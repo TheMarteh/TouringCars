18cec0fe38e4e38817622bb7bc0140c7fda2fd83
Martijn Straathof <martijnfs@me.com>
2022-02-02 16:41:04 +0100

    added  comment

--- TouringCars/Config.cs
+++ TouringCars/Config.cs
@@ -11,7 +11,7 @@ namespace TouringCars
         // the amount of used waypoints for a route
         public static int routePoints = 20;
         // the maximum distance a randomly generated waypoint is away from 0
-        public static int maxDistance = 32;
+        public static int maxDistance = 50;
 
     }
 
@@ -21,6 +21,7 @@ namespace TouringCars
         public static int maxCarFuel = 60;
         // the amount of fuel a car starts with by default
         public static int startingFuel = 25;
+        // determines whether to create a log file.
         public static bool createLogFile = true;
         // Maximum screen width to occupy
         public static int maxScreenWidth = 80;
